1f4ed684e77583e1defbd67ddac13b0992ad0bae
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2025-11-13 23:38:43 -0700

    Creación de paquetes relevantes

---
+++ proyecto_final/src/main/java/joystickmx/negocio/exception/NegocioException.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package joystickmx.negocio.exception;
+
+/**
+ *
+ * @author PC WHITE WOLF
+ */
+public class NegocioException {
+    
+}
